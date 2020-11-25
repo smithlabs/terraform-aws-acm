ab873b9fd808da8980dcbc83a0abec7421414133
Sean Smith <40581796+smithlabs@users.noreply.github.com>
2020-11-24 21:20:30 -0700

    Removed req

--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "domain_name" {
   description = "The domain name you have in Route53"
   type        = string
 }
-
-variable "route53_zone_public_id" {
-  description = "The public ID of the route53 zone"
-  type        = string
-}
